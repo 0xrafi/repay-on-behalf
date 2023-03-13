ca0e41e2f3590540ce28f9df07be22124b49e2f9
Rafi <rafi337161@gmail.com>
2023-03-13 15:57:17 -0500

    continued refactoring/moving

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dcf57f7b465c2132"; }
+/******/ 		__webpack_require__.h = function() { return "e18f737004a4dd6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
