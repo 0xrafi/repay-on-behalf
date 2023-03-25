f761e9e5aabbdac2b57c1945ec47fd104d0cf84c
Rafi <rafi337161@gmail.com>
2023-03-25 01:07:52 -0500

    saving work

--- contracts/test/RepayOnBehalf.t.sol
+++ contracts/test/RepayOnBehalf.t.sol
@@ -34,12 +34,19 @@ contract RepayOnBehalfTest is Test {
 
     function testRepayOnBehalf() public {
         uint256 amount = 20;
+        // Mock the repayBorrowBehalf function call in the cToken contract
+        address cTokenAddress = repayOnBehalf.getCtokenAddress();
+        bytes memory repayBorrowBehalfMockData = abi.encodeWithSignature("repayBorrowBehalf(address,uint256)", owner, amount);
+        vm.mockCall(cTokenAddress, repayBorrowBehalfMockData, abi.encode(uint256(0)));
+
         repayOnBehalf.repayBorrowOnBehalf(owner, amount);
+        (bool success, bytes memory returndata) = cTokenAddress.call(repayBorrowBehalfMockData);
+        require(success, string(returndata));
 
         // Update the mocked borrowBalanceCurrent function to return the new debt
         bytes memory borrowBalanceCurrentMockData = abi.encodeWithSignature("borrowBalanceCurrent(address)", owner);
         bytes memory returnData = abi.encode(existingDebt - amount);
-        address cTokenAddress = repayOnBehalf.getCtokenAddress();
+        
         vm.mockCall(cTokenAddress, borrowBalanceCurrentMockData, returnData);
         assertEq(getDebt(), existingDebt - amount); // Debt should be reduced by the repaid amount
     }
@@ -47,14 +54,21 @@ contract RepayOnBehalfTest is Test {
     function testRepayWithFuzzing(uint256 amount) public {
         uint256 initialDebt = getDebt();
         uint256 repayAmount = (initialDebt >= amount) ? amount : initialDebt;
+        address cTokenAddress = repayOnBehalf.getCtokenAddress();
+        // Mock the repayBorrowBehalf function call in the cToken contract
+        bytes memory repayBorrowBehalfMockData = abi.encodeWithSignature("repayBorrowBehalf(address,uint256)", owner, repayAmount);
+        vm.mockCall(cTokenAddress, repayBorrowBehalfMockData, abi.encode(uint256(0)));
+
         repayOnBehalf.repayBorrowOnBehalf(owner, repayAmount);
+        (bool success, bytes memory returndata) = cTokenAddress.call(repayBorrowBehalfMockData);
+        require(success, string(returndata));
 
         // Update the mocked borrowBalanceCurrent function to return the new debt
         bytes memory borrowBalanceCurrentMockData = abi.encodeWithSignature("borrowBalanceCurrent(address)", owner);
         bytes memory returnData = abi.encode(initialDebt - repayAmount);
-        address cTokenAddress = repayOnBehalf.getCtokenAddress();
+        
         vm.mockCall(cTokenAddress, borrowBalanceCurrentMockData, returnData);
 
-        assertEq(getDebt(), initialDebt - repayAmount); // Debt should decrease by repaid amount
+        assertEq(getDebt(), initialDebt - repayAmount); // Debt should decrease by rep
     }
 }
